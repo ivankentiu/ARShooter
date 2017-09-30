7c55e20e0691e1e29651b1baf2314c66a48ea118
ivantiu <ivankentiu@gmail.com>
2017-10-01 07:29:11 +0800

    egg and bullet

---
+++ ARShooter/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  ARShooter
+//
+//  Created by Ivan Ken Tiu on 01/10/2017.
+//  Copyright © 2017 FinalShift Inc. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    var power: Float = 50
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else { return }
+        guard let pointOfView = sceneView.pointOfView else { return }
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let cameraPosition = orientation + location
+        let bullet = SCNNode(geometry: SCNSphere(radius: 0.1))
+        bullet.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        bullet.position = cameraPosition
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: bullet, options: nil))
+        body.isAffectedByGravity = false
+        bullet.physicsBody = body
+        bullet.physicsBody?.applyForce(SCNVector3(orientation.x * power, orientation.y * power, orientation.z * power), asImpulse: true)
+        self.sceneView.scene.rootNode.addChildNode(bullet)
+    }
+
+    @IBAction func addTargets(_ sender: Any) {
+        self.addEgg(x: 5, y: 0, z: -40)
+        self.addEgg(x: 0, y: 0, z: -40)
+        self.addEgg(x: -5, y: 0, z: -40)
+        
+    }
+    
+    func addEgg(x: Float, y: Float, z: Float) {
+        let eggScene = SCNScene(named: "Media.scnassets/egg.scn")
+        let eggNode = (eggScene?.rootNode.childNode(withName: "egg", recursively: false))!
+        eggNode.position = SCNVector3(x, y, z)
+        eggNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: eggNode, options: nil))
+        self.sceneView.scene.rootNode.addChildNode(eggNode)
+    }
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
